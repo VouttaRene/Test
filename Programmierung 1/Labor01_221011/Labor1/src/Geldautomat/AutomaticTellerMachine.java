035ea40289b2de417f4eb8768642814732038ed8
VouttaRene <45213212+VouttaRene@users.noreply.github.com>
2022-10-17 12:21:43 +0200

    Update AutomaticTellerMachine.java

--- Programmierung 1/Labor01_221011/Labor1/src/Geldautomat/AutomaticTellerMachine.java
+++ Programmierung 1/Labor01_221011/Labor1/src/Geldautomat/AutomaticTellerMachine.java
@@ -70,20 +70,28 @@ public class AutomaticTellerMachine {
 		String card = "none";
 		
 		while(!validCardType) {
-			int cardType = scanner.nextInt();
-			switch(cardType) {
-			case 1:			//Valid Input: Case Girocard
-				card = "Girocard";
-				validCardType = true;
-				break;
-			case 2:			//Valid Input: Case Credit Card
-				card = "credit card";
-				validCardType = true;
-				break;
-			default:		//Invalid Input! Try again
-				System.out.println("Incorrect Card type! Try again!");
-				break;
+			try {
+				int cardType = scanner.nextInt();
+				switch(cardType) {
+				case 1:			//Valid Input: Case Girocard
+					card = "Girocard";
+					validCardType = true;
+					break;
+				case 2:			//Valid Input: Case Credit Card
+					card = "credit card";
+					validCardType = true;
+					break;
+				default:		//Invalid Input! Try again
+					System.out.println("Incorrect Card type! Try again!");
+					break;
+				}
+				
+			}catch(Exception e) {
+				System.out.println("Error!\nTry again!");
+				scanner.next();
+				continue;
 			}
+			
 		}
 		
 		System.out.println("You have selected: " + card + ".");
@@ -93,10 +101,10 @@ public class AutomaticTellerMachine {
 	//This method asks for the withdrawal amount and checks if the amount is acceptable 
 	public static void WithdrawCalc() {		
 		System.out.println("How much do you want to withdraw?");
-		int withdraw = 0;
+		long withdraw = 0L;
 		
 		while(withdraw == 0 || withdraw % 5 != 0) {
-			withdraw = scanner.nextInt();
+			withdraw = scanner.nextLong();
 			if (withdraw % 5 == 0 && withdraw > 0) {
 				Berechnung(withdraw);	//Amount is acceptable and the bills will be calculated.
 			}else {
@@ -109,14 +117,14 @@ public class AutomaticTellerMachine {
 
 	//This method does the calculation of the amount of bills.
 	//At last it will print out the amount of bills.
-	private static void Berechnung(int withdrawal) {
-		int fiveHundred = 0;
-		int twoHundred = 0;
-		int oneHundred = 0;
-		int fifty = 0;
-		int twenty = 0;
-		int ten = 0;
-		int five = 0;
+	private static void Berechnung(long withdrawal) {
+		long fiveHundred = 0;
+		long twoHundred = 0;
+		long oneHundred = 0;
+		long fifty = 0;
+		long twenty = 0;
+		long ten = 0;
+		long five = 0;
 		
 		if(withdrawal >= 500) {
 			fiveHundred = withdrawal / 500;						//By doing a division with integer we get the amount of bills that we can deal out 
