68fb72aa56a29f5f5e30130997f435348c7d0529
VouttaRene <45213212+VouttaRene@users.noreply.github.com>
2023-05-23 12:15:32 +0200

    Aufgabe 3
    Fertig bis auf Kommentare

--- Programmierung2_Aufgabe3/src/aufgabe/SortingEvaluator.java
+++ Programmierung2_Aufgabe3/src/aufgabe/SortingEvaluator.java
@@ -41,15 +41,50 @@ public class SortingEvaluator {
 		}
 		
 		
-		
-		
-		System.out.println(findLowestValue(mergeTimes));
+		printStatistics(selectionTimes, mergeTimes);
 	}
 	
 	private void printStatistics(ArrayList<Long> selectionTimes, ArrayList<Long> mergeTimes) {
-		System.out.println();
+		System.out.println("---SELECTIONSORT STATISTICS---\n");
+		if(arraySize <= 10) {
+			printArray(array);
+		}
+		System.out.println("Performances of the algorithm (in nanoseconds):");
+		for(Long i : selectionTimes)
+			System.out.print(i + " ");
+		
+		System.out.println("\n\nLowest value: " + findLowestValue(selectionTimes) + " nanoseconds"
+				+ "\nHighest value: " + findHighestValue(selectionTimes) + " nanoseconds"
+				+ "\nAverage value: " + findAverageValue(selectionTimes) + " nanoseconds");
+		
+		System.out.println("\n---MERGESORT STATISTICS---\n");
+		if(arraySize <= 10) {
+			printArray(array);
+		}
+		System.out.println("Performances of the algorithm (in nanoseconds):");
+		for(Long i : mergeTimes)
+			System.out.print(i + " ");
+		
+		System.out.println("\n\nLowest value: " + findLowestValue(mergeTimes) + " nanoseconds"
+				+ "\nHighest value: " + findHighestValue(mergeTimes) + " nanoseconds"
+				+ "\nAverage value: " + findAverageValue(mergeTimes) + " nanoseconds");
+		
+		System.out.println("\n---PERFORMANCE COMPARISON---\n");
+		System.out.print("Arraysize: " + arraySize + ", ");
+		performanceComparison(selectionTimes, mergeTimes);
+		
 	}
 	
+	private void performanceComparison(ArrayList<Long> selectionTimes, ArrayList<Long> mergeTimes) {
+		long averageSelectionTime = findAverageValue(selectionTimes);
+		long averageMergeTime = findAverageValue(mergeTimes);
+		
+		if(averageSelectionTime < averageMergeTime)
+			System.out.print("Selectionsort outperformed Mergesort");
+		else
+			System.out.print("Mergesort outperformed Selectionsort");
+	}
+
 	private long findLowestValue(ArrayList<Long> list) {
 		long min = list.get(0);
 		for(long i : list) {
@@ -75,23 +110,28 @@ public class SortingEvaluator {
 		return avg / list.size();
 	}
 	
-	public void sortArray(int[] array) {
-		int marker = array.length - 1;
+	public void printArray(int[] tempArray) {
+		int marker = tempArray.length - 1;
 		while(marker >= 0) {
 			int max = 0;
 			for(int i = 1; i < marker; i++) {
-				if(array[i] > array[max])
+				if(tempArray[i] > tempArray[max])
 					max = i;
 			}
 			
-			swap(array, marker, max);
+			swap(tempArray, marker, max);
 			marker--;
 		}
+		System.out.print("Sorted Array:\n[ ");
+		for(int i : tempArray) {
+			System.out.print(i + " ");
+		}
+		System.out.println("]\n");
 	}
 	
-	private void swap(int[] array, int idx1, int idx2) {
-		int tmp = array[idx1];
-		array[idx1] = array[idx2];
-		array[idx2] = tmp;
+	private void swap(int[] tempArray, int idx1, int idx2) {
+		int tmp = tempArray[idx1];
+		tempArray[idx1] = tempArray[idx2];
+		tempArray[idx2] = tmp;
 	}
 }
